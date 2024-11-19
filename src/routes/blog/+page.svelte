2c42bdd60006e3db82793e14d3ef4a1c488d405f
Joaquin Saez <ximosa@gmail.com>
2024-11-19 11:47:15 +0100

    Update +page.svelte

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -130,7 +130,9 @@
         border-radius: 8px;
         box-shadow: 0 2px 4px rgba(0,0,0,0.1);
     }
-
+ .post h2{
+        font-size: 1.5rem;
+    }
     .post-image img {
         width: 100%;
         height: 200px;
